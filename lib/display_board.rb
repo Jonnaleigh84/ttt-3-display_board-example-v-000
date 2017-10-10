0075ab02567ff6e22e793ae48a68c18d1b947d44
Jonna Leigh <zanycandlecreations@gmail.com>
2017-10-10 14:14:52 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,13 +2,3 @@
 def display_board
   puts "A Tic Tac Toe Board"
 end
-
-expect(output).to include("   ")
-  expected "A Tic Tac Toe Board\n" to include " <pre>" "</pre>
-  <pre>   |   |   </pre>
-  <pre>'-----------'</pre>
-  <pre>'-----------'</pre>
-  <pre>'-----------'</pre>
-   "
-  Diff:
-  @@ -1,2 +1,2 @@
